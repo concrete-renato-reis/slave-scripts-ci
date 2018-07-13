6af38b725c2ee844c12df4fab055a85244781e97
bcg <bcg@Mac-mini-de-bcg.local>
2018-07-13 17:00:11 -0300

    upload scripts

---
+++ start_appium.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+### Script for Kill process on Linux ###
+### Created by Marcio G Chahad - version 1.0 - 070618 ###
+
+### Process Name Definition ###
+PROCESS="/usr/local/bin/appium"
+
+### Kill process ###
+CHECK_PROCESS=`ps aux|grep "$PROCESS" |grep -v grep|head -1|awk '{print $2}'`
+if [ ! -z $CHECK_PROCESS ]; then
+  echo "Process is running - ok"
+  else
+/usr/local/bin/node /usr/local/bin/appium &
+fi
---
+++ start_device.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Script for start Android virtual device and Appium Server ###
+
+### LIST DEVICES ###
+
+NEXUS="Nexus_5X_API_22_1"
+
+### STOP DEVICE ###
+
+CHECK_EMULATOR=`ps -aef | fgrep   "Nexus_5X_API_22_1" | grep -v grep | head -n 1 | awk '{print $2}'`
+
+if [ ! -z $CHECK_EMULATOR ]; then
+
+   kill -9 $CHECK_EMULATOR
+
+   else
+
+   echo "Emulador Offline"
+
+fi
+
+### START DEVICE ###
+
+$HOME/Library/Android/sdk/tools/emulator -avd "$NEXUS" &
+

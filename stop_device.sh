08c72389ac8fd8e8c452e6d8cfb74d0b9f2a3d9a
bcg <bcg@Mac-mini-de-bcg.local>
2018-08-06 14:03:41 -0300

    upload script

---
+++ stop_device.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### LIST DEVICES ###
+
+NEXUS="Nexus_5X_API_22_1"
+
+### STOP DEVICE ###
+CHECK_EMULATOR=`ps -aef | fgrep   "Nexus_5X_API_22_1" | grep -v grep  | head -n 1 | awk '{print $2}'`
+
+if [ ! -z $CHECK_EMULATOR ]; then
+
+   kill -9 $CHECK_EMULATOR
+
+   else
+
+   echo "Emulador Offline"
+
+fi
